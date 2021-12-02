221d078dccb17e06ee09bc4b1d167b3a0a24730d
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-12-02 14:02:20 +0300

    Task 0

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 
+
 char *lines_check(char *buffer, unsigned int line_number)
 {
 	char *token = NULL, *num_t = NULL;
@@ -31,4 +32,45 @@ char *lines_check(char *buffer, unsigned int line_number)
 		var_glob[0] = atoi(num_t);
 	}
 	return (token);
+}
+
+/**
+ *main - execute the monty interpreter
+ */
+
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
+	FILE *file;
+	char *buffer = NULL, *command_f = NULL;
+	size_t size = 0;
+	unsigned int line_number = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &size, file) != EOF)
+	{
+		line_number++;
+		if (strlen(buffer) == 1 || strspn(buffer, " \t\n") == strlen(buffer))
+			continue;
+		command_f = lines_check(buffer, line_number);
+		if (command_f == NULL)
+			break;
+		functions_monty(&stack, command_f, line_number);
+	}
+	free(buffer);
+	free_malloc(stack);
+	fclose(file);
+	if (var_glob[1] == 1)
+		exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
