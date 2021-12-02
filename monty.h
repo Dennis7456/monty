8e89ddc863ce2ed2c1ff64a053ff0c9dc4f3717d
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-12-02 14:03:30 +0300

    Task 0

--- monty.h
+++ monty.h
@@ -42,4 +42,15 @@ typedef struct instruction_s
 
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
+void functions_monty(stack_t **stack, char *command_f, unsigned int line_numb);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void free_malloc(stack_t *head);
 #endif
